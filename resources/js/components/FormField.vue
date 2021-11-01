468b6a92617fd59851f6be43eaea6408ecdf093d
Kim Ellefsen <kim@ellefsen.me>
2021-11-01 14:01:34 +0100

    Insert flexible block at specific position

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,59 +1,60 @@
 <template>
-    <component
-        :dusk="field.attribute"
-        :is="field.fullWidth ? 'full-width-field' : 'default-field'"
+  <component
+    :dusk="field.attribute"
+    :is="field.fullWidth ? 'full-width-field' : 'default-field'"
+    :field="field"
+    :errors="errors"
+    full-width-content
+    :show-help-text="showHelpText">
+    <template slot="field">
+      <div
+        v-if="order.length > 0">
+        <form-nova-flexible-content-group
+          v-for="(group, index) in orderedGroups"
+          :dusk="field.attribute + '-' + index"
+          :key="group.key"
+          :field="field"
+          :group="group"
+          :index="index"
+          :resource-name="resourceName"
+          :resource-id="resourceId"
+          :resource="resource"
+          :errors="errors"
+          :layouts="layouts"
+          :limit-per-layout-counter="limitPerLayoutCounter"
+          @move-up="moveUp(group.key)"
+          @move-down="moveDown(group.key)"
+          @remove="remove(group.key)"
+          @addGroup="addGroupFromIndex"
+        />
+      </div>
+
+      <component
+        :layouts="layouts"
+        :is="field.menu.component"
         :field="field"
+        :limit-counter="limitCounter"
+        :limit-per-layout-counter="limitPerLayoutCounter"
         :errors="errors"
-        full-width-content
-        :show-help-text="showHelpText">
-        <template slot="field">
-
-            <div
-                v-if="order.length > 0">
-                <form-nova-flexible-content-group
-                    v-for="(group, index) in orderedGroups"
-                    :dusk="field.attribute + '-' + index"
-                    :key="group.key"
-                    :field="field"
-                    :group="group"
-                    :index="index"
-                    :resource-name="resourceName"
-                    :resource-id="resourceId"
-                    :resource="resource"
-                    :errors="errors"
-                    @move-up="moveUp(group.key)"
-                    @move-down="moveDown(group.key)"
-                    @remove="remove(group.key)"
-                />
-            </div>
-
-            <component
-                :layouts="layouts"
-                :is="field.menu.component"
-                :field="field"
-                :limit-counter="limitCounter"
-                :limit-per-layout-counter="limitPerLayoutCounter"
-                :errors="errors"
-                :resource-name="resourceName"
-                :resource-id="resourceId"
-                :resource="resource"
-                @addGroup="addGroup($event)"
-            />
-
-        </template>
-    </component>
+        :resource-name="resourceName"
+        :resource-id="resourceId"
+        :resource="resource"
+        @addGroup="addGroup($event)"
+      />
+    </template>
+  </component>
 </template>
 
 <script>
 
-import FullWidthField from './FullWidthField';
-import { FormField, HandlesValidationErrors } from 'laravel-nova';
-import Group from '../group';
+import FullWidthField from "./FullWidthField"
+import { FormField, HandlesValidationErrors } from "laravel-nova"
+import Group from "../group"
 
 export default {
     mixins: [FormField, HandlesValidationErrors],
 
-    props: ['resourceName', 'resourceId', 'resource', 'field'],
+    props: ["resourceName", "resourceId", "resource", "field"],
 
     components: { FullWidthField },
 
@@ -63,34 +64,34 @@ export default {
         },
         orderedGroups() {
             return this.order.reduce((groups, key) => {
-                groups.push(this.groups[key]);
-                return groups;
-            }, []);
+                groups.push(this.groups[key])
+                return groups
+            }, [])
         },
 
         limitCounter() {
             if (this.field.limit === null || typeof(this.field.limit) == "undefined") {
-              return null;
+              return null
             }
 
             // if all layouts reached their limitPerLayout, remove the "Add" button
             if (Object.values(this.limitPerLayoutCounter).reduce((a, b) => a + b, 0) <= 0) {
-                return 0;
+                return 0
             }
 
-            return this.field.limit - Object.keys(this.groups).length;
+            return this.field.limit - Object.keys(this.groups).length
         },
 
         limitPerLayoutCounter() {
-            let count = {};
+            let count = {}
             this.layouts.forEach(layout => count[layout.name] = layout.limit)
             if (Object.keys(this.groups).length > 0) {
                 Object.entries(this.groups).forEach(
                     group => count[group[1].name] === null ? null : count[group[1].name]--
-                );
+                )
             }
 
-            return count;
+            return count
         },
     },
 
@@ -99,7 +100,7 @@ export default {
             order: [],
             groups: {},
             files: {}
-        };
+        }
     },
 
     methods: {
@@ -107,42 +108,42 @@ export default {
          * Set the initial, internal value for the field.
          */
         setInitialValue() {
-            this.value = this.field.value || [];
-            this.files = {};
+            this.value = this.field.value || []
+            this.files = {}
 
-            this.populateGroups();
+            this.populateGroups()
         },
 
         /**
          * Fill the given FormData object with the field's internal value.
          */
         fill(formData) {
-            let key, group;
+            let key, group
 
-            this.value = [];
-            this.files = {};
+            this.value = []
+            this.files = {}
 
             for (var i = 0; i < this.order.length; i++) {
-                key = this.order[i];
-                group = this.groups[key].serialize();
+                key = this.order[i]
+                group = this.groups[key].serialize()
 
                 // Only serialize the group's non-file attributes
                 this.value.push({
                     layout: group.layout,
                     key: group.key,
                     attributes: group.attributes
-                });
+                })
 
                 // Attach the files for formData appending
-                this.files = {...this.files, ...group.files};
+                this.files = {...this.files, ...group.files}
             }
 
-            this.appendFieldAttribute(formData, this.field.attribute);
-            formData.append(this.field.attribute, this.value.length ? JSON.stringify(this.value) : '');
+            this.appendFieldAttribute(formData, this.field.attribute)
+            formData.append(this.field.attribute, this.value.length ? JSON.stringify(this.value) : "")
 
             // Append file uploads
             for(let file in this.files) {
-                formData.append(file, this.files[file]);
+                formData.append(file, this.files[file])
             }
         },
 
@@ -150,33 +151,33 @@ export default {
          * Register given field attribute into the parsable flexible fields register
          */
         appendFieldAttribute(formData, attribute) {
-            let registered = [];
+            let registered = []
 
-            if(formData.has('___nova_flexible_content_fields')) {
-                registered = JSON.parse(formData.get('___nova_flexible_content_fields'));
+            if(formData.has("___nova_flexible_content_fields")) {
+                registered = JSON.parse(formData.get("___nova_flexible_content_fields"))
             }
 
-            registered.push(attribute);
+            registered.push(attribute)
 
-            formData.set('___nova_flexible_content_fields', JSON.stringify(registered));
+            formData.set("___nova_flexible_content_fields", JSON.stringify(registered))
         },
 
         /**
          * Update the field's internal value.
          */
         handleChange(value) {
-            this.value = value || [];
-            this.files = {};
+            this.value = value || []
+            this.files = {}
 
-            this.populateGroups();
+            this.populateGroups()
         },
 
         /**
          * Set the displayed layouts from the field's current value
          */
         populateGroups() {
-            this.order.splice(0, this.order.length);
-            this.groups = {};
+            this.order.splice(0, this.order.length)
+            this.groups = {}
 
             for (var i = 0; i < this.value.length; i++) {
                 this.addGroup(
@@ -184,7 +185,7 @@ export default {
                     this.value[i].attributes,
                     this.value[i].key,
                     this.field.collapsed
-                );
+                )
             }
         },
 
@@ -192,57 +193,70 @@ export default {
          * Retrieve layout definition from its name
          */
         getLayout(name) {
-            if(!this.layouts) return;
-            return this.layouts.find(layout => layout.name == name);
+            if(!this.layouts) return
+            return this.layouts.find(layout => layout.name == name)
         },
 
         /**
          * Append the given layout to flexible content's list
          */
         addGroup(layout, attributes, key, collapsed) {
-            if(!layout) return;
+            if(!layout) return
 
-            collapsed = collapsed || false;
+            collapsed = collapsed || false
 
             let fields = attributes || JSON.parse(JSON.stringify(layout.fields)),
-                group = new Group(layout.name, layout.title, fields, this.field, key, collapsed);
+                group = new Group(layout.name, layout.title, fields, this.field, key, collapsed)
 
-            this.$set(this.groups, group.key, group);
-            this.order.push(group.key);
+            this.$set(this.groups, group.key, group)
+            this.order.push(group.key)
+        },
+
+        /**
+         * Append the given layout to flexible content's list
+         */
+        addGroupFromIndex(event, index) {
+            this.addGroup(event)
+            let lastElementIndex = this.order.length - 1
+
+            let to = index + 1
+            let from = lastElementIndex
+
+            this.order.splice(to, 0, this.order.splice(from, 1)[0])
         },
 
         /**
          * Move a group up
          */
         moveUp(key) {
-            let index = this.order.indexOf(key);
+            let index = this.order.indexOf(key)
 
-            if(index <= 0) return;
+            if(index <= 0) return
 
-            this.order.splice(index - 1, 0, this.order.splice(index, 1)[0]);
+            this.order.splice(index - 1, 0, this.order.splice(index, 1)[0])
         },
 
         /**
          * Move a group down
          */
         moveDown(key) {
-            let index = this.order.indexOf(key);
+            let index = this.order.indexOf(key)
 
-            if(index < 0 || index >= this.order.length - 1) return;
+            if(index < 0 || index >= this.order.length - 1) return
 
-            this.order.splice(index + 1, 0, this.order.splice(index, 1)[0]);
+            this.order.splice(index + 1, 0, this.order.splice(index, 1)[0])
         },
 
         /**
          * Remove a group
          */
         remove(key) {
-            let index = this.order.indexOf(key);
+            let index = this.order.indexOf(key)
 
-            if(index < 0) return;
+            if(index < 0) return
 
-            this.order.splice(index, 1);
-            this.$delete(this.groups, key);
+            this.order.splice(index, 1)
+            this.$delete(this.groups, key)
         }
     }
 }
--- resources/js/components/FormGroup.vue
+++ resources/js/components/FormGroup.vue
@@ -1,124 +1,166 @@
 <template>
-    <div class="relative flex bg-white mb-4 pb-1" :id="group.key">
-        <div class="z-10 bg-white border-t border-l border-b border-60 h-auto pin-l pin-t rounded-l self-start w-8">
-            <button
-                dusk="expand-group"
-                type="button"
-                class="group-control btn border-r border-40 w-8 h-8 block"
-                :title="__('Expand')"
-                @click.prevent="expand"
-                v-if="collapsed">
-                <icon class="align-top" type="plus-square" width="16" height="16" view-box="0 0 24 24" />
-            </button>
-            <div v-if="!collapsed">
-                <button
-                    dusk="collapse-group"
-                    type="button"
-                    class="group-control btn border-r border-40 w-8 h-8 block"
-                    :title="__('Collapse')"
-                    @click.prevent="collapse">
-                    <icon class="align-top" type="minus-square" width="16" height="16" view-box="0 0 24 24" />
-                </button>
-                <div v-if="!readonly">
-                    <button
-                        dusk="move-up-group"
-                        type="button"
-                        class="group-control btn border-t border-r border-40 w-8 h-8 block"
-                        :title="__('Move up')"
-                        @click.prevent="moveUp">
-                        <icon type="arrow-up" view-box="0 0 8 4.8" width="10" height="10" />
-                    </button>
-                    <button
-                        dusk="move-down-group"
-                        type="button"
-                        class="group-control btn border-t border-r border-40 w-8 h-8 block"
-                        :title="__('Move down')"
-                        @click.prevent="moveDown">
-                        <icon type="arrow-down" view-box="0 0 8 4.8" width="10" height="10" />
-                    </button>
-                    <button
-                        dusk="delete-group"
-                        type="button"
-                        class="group-control btn border-t border-r border-40 w-8 h-8 block"
-                        :title="__('Delete')"
-                        @click.prevent="confirmRemove">
-                        <icon type="delete" view-box="0 0 20 20" width="16" height="16" />
-                    </button>
-                    <portal to="modals">
-                        <delete-flexible-content-group-modal
-                            v-if="removeMessage"
-                            @confirm="remove"
-                            @close="removeMessage=false"
-                            :message="field.confirmRemoveMessage"
-                            :yes="field.confirmRemoveYes"
-                            :no="field.confirmRemoveNo"
-                        />
-                    </portal>
-                </div>
-            </div>
+  <div
+    class="relative flex bg-white mb-4 pb-1"
+    :id="group.key">
+    <div class="z-10 bg-white border-t border-l border-b border-60 h-auto pin-l pin-t rounded-l self-start w-8">
+      <button
+        dusk="expand-group"
+        type="button"
+        class="group-control btn border-r border-40 w-8 h-8 block"
+        :title="__('Expand')"
+        @click.prevent="expand"
+        v-if="collapsed">
+        <icon
+          class="align-top"
+          type="plus-square"
+          width="16"
+          height="16"
+          view-box="0 0 24 24" />
+      </button>
+      <div v-if="!collapsed">
+        <button
+          dusk="collapse-group"
+          type="button"
+          class="group-control btn border-r border-40 w-8 h-8 block"
+          :title="__('Collapse')"
+          @click.prevent="collapse">
+          <icon
+            class="align-top"
+            type="minus-square"
+            width="16"
+            height="16"
+            view-box="0 0 24 24" />
+        </button>
+        <div v-if="!readonly">
+          <button
+            dusk="move-up-group"
+            type="button"
+            class="group-control btn border-t border-r border-40 w-8 h-8 block"
+            :title="__('Move up')"
+            @click.prevent="moveUp">
+            <icon
+              type="arrow-up"
+              view-box="0 0 8 4.8"
+              width="10"
+              height="10" />
+          </button>
+          <button
+            dusk="move-down-group"
+            type="button"
+            class="group-control btn border-t border-r border-40 w-8 h-8 block"
+            :title="__('Move down')"
+            @click.prevent="moveDown">
+            <icon
+              type="arrow-down"
+              view-box="0 0 8 4.8"
+              width="10"
+              height="10" />
+          </button>
+          <button
+            dusk="delete-group"
+            type="button"
+            class="group-control btn border-t border-r border-40 w-8 h-8 block"
+            :title="__('Delete')"
+            @click.prevent="confirmRemove">
+            <icon
+              type="delete"
+              view-box="0 0 20 20"
+              width="16"
+              height="16" />
+          </button>
+          <portal to="modals">
+            <delete-flexible-content-group-modal
+              v-if="removeMessage"
+              @confirm="remove"
+              @close="removeMessage=false"
+              :message="field.confirmRemoveMessage"
+              :yes="field.confirmRemoveYes"
+              :no="field.confirmRemoveNo"
+            />
+          </portal>
         </div>
-        <div class="-mb-1 flex flex-col min-h-full w-full">
-            <div :class="titleStyle" v-if="group.title">
-                <div class="leading-normal py-1 px-8"
-                    :class="{'border-b border-40': !collapsed}">
-                    <p class="text-80">
-                      <span class="mr-4 font-semibold">#{{ index + 1 }}</span>
-                      {{ group.title }}
-                    </p>
-                </div>
-            </div>
-            <div :class="containerStyle">
-                <component
-                    v-for="(item, index) in group.fields"
-                    :key="index"
-                    :is="'form-' + item.component"
-                    :resource-name="resourceName"
-                    :resource-id="resourceId"
-                    :resource="resource"
-                    :field="item"
-                    :errors="errors"
-                    :show-help-text="item.helpText != null"
-                    :class="{ 'remove-bottom-border': index == group.fields.length - 1 }"
-                />
-            </div>
-        </div>
+      </div>
     </div>
+    <div class="-mb-1 flex flex-col min-h-full w-full">
+      <div
+        :class="titleStyle"
+        v-if="group.title">
+        <div
+          class="leading-normal py-1 px-8"
+          :class="{'border-b border-40': !collapsed}">
+          <p class="text-80">
+            <span class="mr-4 font-semibold">#{{ index + 1 }}</span>
+            {{ group.title }}
+          </p>
+        </div>
+      </div>
+      <div :class="containerStyle">
+        <component
+          v-for="(item, index) in group.fields"
+          :key="index"
+          :is="'form-' + item.component"
+          :resource-name="resourceName"
+          :resource-id="resourceId"
+          :resource="resource"
+          :field="item"
+          :errors="errors"
+          :show-help-text="item.helpText != null"
+          :class="{ 'remove-bottom-border': index == group.fields.length - 1 }"
+        />
+      </div>
+
+      <div class="mt-4">
+        <component
+          :layouts="layouts"
+          :is="`flexible-drop-menu`"
+          :limit-counter="null"
+          :limit-per-layout-counter="limitPerLayoutCounter"
+          :field="field"
+          :errors="errors"
+          :resource-name="resourceName"
+          :resource-id="resourceId"
+          :resource="resource"
+          @addGroup="$emit('addGroup', $event, index)"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { BehavesAsPanel } from 'laravel-nova';
+import { BehavesAsPanel } from "laravel-nova"
 
 export default {
     mixins: [BehavesAsPanel],
 
-    props: ['errors', 'group', 'index', 'field'],
+    props: ["errors", "group", "index", "field", "layouts", "limitPerLayoutCounter"],
 
     data() {
         return {
             removeMessage: false,
             collapsed: this.group.collapsed,
             readonly: this.group.readonly,
-        };
+        }
     },
 
     computed: {
         titleStyle() {
-            let classes = ['border-t', 'border-r', 'border-60', 'rounded-tr-lg'];
+            let classes = ["border-t", "border-r", "border-60", "rounded-tr-lg"]
             if (this.collapsed) {
-                classes.push('border-b rounded-br-lg');
+                classes.push("border-b rounded-br-lg")
             }
-            return classes;
+            return classes
         },
         containerStyle() {
-            let classes = ['flex-grow', 'border-b', 'border-r', 'border-l', 'border-60', 'rounded-b-lg'];
+            let classes = ["flex-grow", "border-b", "border-r", "border-l", "border-60", "rounded-b-lg"]
             if(!this.group.title) {
-                classes.push('border-t');
-                classes.push('rounded-tr-lg');
+                classes.push("border-t")
+                classes.push("rounded-tr-lg")
             }
             if (this.collapsed) {
-                classes.push('hidden');
+                classes.push("hidden")
             }
-            return classes;
+            return classes
         }
     },
 
@@ -127,21 +169,21 @@ export default {
          * Move this group up
          */
         moveUp() {
-            this.$emit('move-up');
+            this.$emit("move-up")
         },
 
         /**
          * Move this group down
          */
         moveDown() {
-            this.$emit('move-down');
+            this.$emit("move-down")
         },
 
         /**
          * Remove this group
          */
         remove() {
-            this.$emit('remove');
+            this.$emit("remove")
         },
 
         /**
@@ -149,7 +191,7 @@ export default {
          */
         confirmRemove() {
             if(this.field.confirmRemove){
-                this.removeMessage = true;
+                this.removeMessage = true
             } else {
                 this.remove()
             }
@@ -159,14 +201,14 @@ export default {
          * Expand fields
          */
         expand() {
-            this.collapsed = false;
+            this.collapsed = false
         },
 
         /**
          * Collapse fields
          */
         collapse() {
-            this.collapsed = true;
+            this.collapsed = true
         }
     },
 }
